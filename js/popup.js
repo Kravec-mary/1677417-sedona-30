55c24966b518e51a1facb2d47628abcee00d8d3f
Kravec-mary <76391242+Kravec-mary@users.noreply.github.com>
2021-03-09 23:55:20 +0300

    правки

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,15 @@
-const btnClose = document.querySelector('.btn');
-      const btnPopup = document.querySelector('.choice-date');
+       const btnClose = document.querySelector('.btn');
+       const btnPopup = document.querySelector('.choice-date');
+      
+       btnClose.addEventListener('click', function (evt) {
+        evt.preventDefault();
+       btnPopup.classList.toggle('choice-date-close');
+      //  btnPopup.classList.add('form-show');
+ });
+       const popup = document.querySelector('.section-choice-date');
+       const form = document.querySelector('form');
+       form.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        console.log('Отправляем форму');
+       });
 
-      btnClose.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      btnPopup.classList.toggle("choice-date-close");
-});
